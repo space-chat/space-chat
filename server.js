92d5ad00cbd787c3729b3c8c9ca7fbe7427fa764
Keziyah <keziyahlewis@gmail.com>
2017-05-06 17:31:52 -0400

    working on getting translation from firebase

--- server.js
+++ server.js
@@ -57,30 +57,32 @@ isConnected.on('value', snap => console.log('Firebase',
 
 const translateMessage = snapshot => {
   // Text to translate, e.g. "Hello, world!"
-  const {text} = snapshot.val()
+  const { text } = snapshot.val()
   // Two letters to represent target language, e.g. "ru"
   const target = 'id'
   // If no 'text' key, ignore this entry, exit the function
   if (!text) return
-  
+
   // Translates the text into the target language
   // typeof text: 'text1' || [text1, text2, ...]
   console.log('translating "%s" into %s', text, target)
+  let translation = ""
   return translate.translate(text, target)
     .then((results) => {
       // Grabs relevant value from arr returned by API
-      let translations = results[0]
-      // Standardizes value as array
-      translations = Array.isArray(translations) 
-        ? translations 
-        : [translations]
+      translation = results[0]
       // Pushes new entry to database
       return Promise.all(
         [snapshot.ref.parent.push({
           type: 'TRANSLATION',
           done: true,
-          [target]: translations}),
+          [target]: translation
+        }),
         snapshot.ref.update({ done: true })])
+    })
+    .then(() => {
+      console.log("translation", translation)
+      return translation
     })
     .catch((err) => {
       console.error('ERROR:', err)
@@ -88,26 +90,29 @@ const translateMessage = snapshot => {
 }
 
 // Processes all unprocessed messages in current room
-const processRoom = ref => {
-  // Finds all messages not yet translated
-  ref.orderByChild('done')
-    .equalTo(null)
-    // Sets temporary listener
-    .once('value')
-    // Sends messages to google translate API
-    .then(snap => {
-      const val = snap.val()
-      console.log(val)
-      return Promise.all(
-        Object.keys(val)
-          // Sends fake snapshot to translateMessage fxn
-          .map(key => translateMessage({
-            ref: snap.ref.child(key),
-            val() { return val[key] }
-          }))
-        )
-    })
-}
+// const processRoom = ref => {
+//   // Finds all messages not yet translated
+//   ref.orderByChild('done')
+//     .equalTo(null)
+//     .limitToFirst(1)
+//     // Sets temporary listener
+//     .once('value')
+//     // Sends messages to google translate API
+//     .then(snap => {
+//       // const val = snap.val()
+//       console.log("snap", snap)
+//       return translateMessage(snap)
+//       //  return Promise.all(
+//       //   Object.keys(val)
+//       //     // Sends fake snapshot to translateMessage fxn
+//       //     .map(key => translateMessage({
+//       //       ref: snap.ref.child(key),
+//       //       val() { return val[key] }
+//       //     }))
+//       //   )
+//         // .then(translations => translations)
+//     })
+// }
 
 const onceWeAreLoggedIn = new Promise((resolve, reject) => {
   const unsub = firebase.auth().onAuthStateChanged(user => {
@@ -119,34 +124,46 @@ const onceWeAreLoggedIn = new Promise((resolve, reject) => {
 })
 
 // const processIncomingMessage = data => {
-  
+
 // }
 
 // Pings ghost server hosted on Heroku
 require('express')()
-.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5000")
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
-  if ("OPTIONS" == req.method) {
-    res.sendStatus(200); 
-  } else {
-    next()
-  }
-})
-.use(bodyParser.json())
-.use(bodyParser.urlencoded({extended: false}))
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5000")
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
+    if ("OPTIONS" == req.method) {
+      res.sendStatus(200);
+    } else {
+      next()
+    }
+  })
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
   .post('/:roomId', (req, res) => {
     console.log("REQ", req.body)
     onceWeAreLoggedIn
-    .then(() => firebase.database().ref('rooms').child(req.params.roomId).push({
-      text: req.body.text
-    }))
-      .then(() => processRoom(
-        firebase.database().ref('rooms')
-          .child(req.params.roomId)
-      ))
-      .then(() => res.send(""))})
+      .then(() => firebase.database().ref('rooms').child(req.params.roomId).push({
+        text: req.body.text
+      }))
+      .then(() =>
+        firebase.database().ref('rooms').child(req.params.roomId)
+          .orderByChild('done')
+          .equalTo(null)
+          .limitToLast(1)
+          // Sets temporary listener
+          .once('value')
+          .then(snap => {
+          //   translateMessage({
+          //   ref: snap.ref.child(key), 
+          //   val() {return val[key]}
+          // })
+          console.log("SNAP.REF", snap.ref)
+          })
+      )
+      .then((translation) => console.log("TRANSLATIONS", translation))
+  })
   .listen(process.env.PORT || 9999)
 
 
