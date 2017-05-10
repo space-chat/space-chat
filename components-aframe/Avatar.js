398fb37ab33382b5ca38244cf8657b9bcac354a4
ssundby <stefanie.sundby@gmail.com>
2017-05-10 16:35:22 -0400

    added components-aframe folder with AssetsLoader.js and Avatar.js

---
+++ components-aframe/Avatar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// props passed from Room (or 'Scene') component
+// the Room is an A-frame smart component. Objects in the room are dumb components.
+export default (props) => (
+	// define entity. If we want the position to be responsive to sentiment, the x, y, z coordinates can be variables, as demonstrated here.
+	<a entity id="avatar" position={`${props.x} ${props.y} ${props.z}`}></a>
+	)
