789bdc1a3f887e90051e61e1acb2f9be01616526
Keziyah <keziyahlewis@gmail.com>
2017-05-12 01:54:02 -0400

    bubble visuals complete. to do next - sentiment analysis and making the component reusable

--- app/components/Bubbles.jsx
+++ app/components/Bubbles.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import * as THREE from 'three';
 
 import { initScene, makeBubbles, animate } from './bubbles.js'
 
@@ -11,18 +10,48 @@ export default class Bubbles extends Component {
         animate()
     }
 
-    render() { 
+    //to do later:
+    //Make this component customizable so that it could take in
+    //different skies and bubble faces
+    //Make bubbles.js easier to read/look at
+    //Hook up sentiment analysis
+    //Over the weekend, work on a solar system component
+
+    //For sentiment analysis:
+
+
+//bubbles move in circle, revolving around something
+
+    //Sentiment: sphere color or metalness
+    //Personality: 
+        //extraversion: bubbles increase in size
+        //conscientiousness
+        //openness
+        //agreeableness
+    //Emotion
+        //joy: bubbles swirl around above in a pattern. 
+        //anger: bubbles move very fast
+        //fear: bubbles stand still
+        //sadness: bubbles drop to a low altitude, and they start to disappear
+        //surprise: bubbles light up in all kinds of cool colors. 
+
+    render() {
         return (
             <div>
                 <a-scene vr-mode-ui="enabled: true">
-                    <a-entity id="bubbleCamera" camera="userHeight: 1.6" look-controls></a-entity>
+                    <a-entity id="bubbleCamera" camera="userHeight: 1.6" look-controls
+                        orbit-controls="autoRotate: false; target: #pink; enableDamping: true; dampingFactor: 0.25; rotateSpeed:0.14; minDistance:3; maxDistance:15;" mouse-cursor="">
+                    </a-entity>
                     <a-assets>
                         <img id="flowerSky" src="blossoms.jpg" />
                     </a-assets>
-                    <a-sphere position="-1 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+                    <a-sphere position="-1 1.25 -5" radius="0.01" color="#EF2D5E" id="pink"></a-sphere>
                     <a-sky src="#flowerSky"></a-sky>
                 </a-scene>
             </div>
         )
     }
 }
+
+//Though we can't see the pink sphere, I'm still using it for the
+//orbit controls. 
--- app/components/bubbles.js
+++ app/components/bubbles.js
@@ -1,101 +1,75 @@
+//This component controls sphere rendering and animations of Bubbles.jsx. 
+import * as THREE from 'three'
+
 //This code is based on Three.js' example here: https://github.com/mrdoob/three.js/blob/master/examples/webgl_effects_anaglyph.html
 var spheres = [];
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
+var width = window.innerWidth || 2;
+var height = window.innerHeight || 2;
 var directionalLight, pointLight;
 var mouseX = 0;
 var mouseY = 0;
 
 
-
-export const initScene = () => {
+function initScene() {
 	var camera = document.getElementById('bubbleCamera')
 	camera.setAttribute('fov', 60) //field of view
-	camera.setAttribute('aspect', window.innerWidth/window.innerHeight) //aspect
+	camera.setAttribute('aspect', window.innerWidth / window.innerHeight) //aspect
 	camera.setAttribute('near', 0.01) //near
-	camera.setAttribute('far', 100) //far
-	camera.setAttribute('position', {z: 5})
+	camera.setAttribute('far', 1000) //far
+	camera.setAttribute('position', { z: 5 })
 	camera.setAttribute('focalLength', 3)
 }
 
-export const makeBubbles = () => {
-for (var i = 0; i < 40; i++) {
-	var sphere = document.createElement('a-sphere')
-	let x = Math.random() * 10 - 5;
-	let y = Math.random() * 10 - 5;
-	let z = Math.random() * 10 - 5;
-	sphere.setAttribute('position', {x: x, y: y, z: z})
-	let scale = Math.random() * 1 + 0.3
-	sphere.setAttribute('scale', {x: scale, y: scale, z: scale})
-	document.querySelector('a-scene').appendChild(sphere);
-	spheres.push(sphere);
-
-	window.addEventListener( 'resize', onWindowResize, false );
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false )
-}
-}
-
-	function onWindowResize() {
-		var camera = document.getElementById('bubbleCamera')
-		windowHalfX = window.innerWidth / 2;
-		windowHalfY = window.innerHeight / 2;
-		camera.setAttribute('aspect', window.innerWidth / window.innerHeight)
-		//effect.setSize( window.innerWidth, window.innerHeight );
+function makeBubbles() {
+	for (var i = 0; i < 200; i++) {
+		var sphere = document.createElement('a-sphere')
+		let x = Math.random() * 10;
+		let y = Math.random() * 10;
+		let z = Math.random() * 10;
+		sphere.setAttribute('material', "src:#flowerSky; roughness: 0.01")
+		sphere.setAttribute('position', { x: x, y: y, z: z })
+		let scale = Math.random() * 1 + 0.2   //change for bubble size
+		sphere.setAttribute('scale', { x: scale, y: scale, z: scale })
+		document.querySelector('a-scene').appendChild(sphere);
+		spheres.push(sphere);
 	}
 
-	function onDocumentMouseMove(event) {
-				mouseX = ( event.clientX - windowHalfX ) / 100;
-				mouseY = ( event.clientY - windowHalfY ) / 100;
+	window.addEventListener('resize', onWindowResize, false);
+	document.addEventListener('mousemove', onDocumentMouseMove, false)
+}
+
+function onWindowResize() {
+	var camera = document.getElementById('bubbleCamera')
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	camera.setAttribute('aspect', window.innerWidth / window.innerHeight)
+}
+
+function onDocumentMouseMove(event) {
+	mouseX = (event.clientX - windowHalfX) / 100;
+	mouseY = (event.clientY - windowHalfY) / 100;
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	render();
+}
+
+function render() {
+	var camera = document.getElementById('bubbleCamera')
+	var timer = 0.0001 * Date.now(); //Change the number for bubble speed
+	let curr = camera.getAttribute("position")
+	let addx = curr.x + ((mouseX - curr.x) * .05)
+	let addy = curr.y + ((- mouseY - curr.y) * .05)
+	camera.setAttribute('position', { x: addx, y: addy, z: 5 })
+
+	for (var i = 0, il = spheres.length; i < il; i++) {
+		var sphere = spheres[i];
+		sphere.setAttribute('position', { x: 7 * Math.cos(timer + i) }) //Change for bubble distance
+		sphere.setAttribute('position', { y: 7 * Math.sin(timer + i * 1.1) })
 	}
+}
 
-				var path = "textures/cube/pisa/";
-				var format = '.png';
-				var urls = [
-					path + 'px' + format, path + 'nx' + format,
-					path + 'py' + format, path + 'ny' + format,
-					path + 'pz' + format, path + 'nz' + format
-				];
-				var textureCube = new THREE.CubeTextureLoader().load( urls );
-				// scene = new THREE.Scene();
-			// 	scene.background = textureCube;
-			// 	var geometry = new THREE.SphereBufferGeometry( 0.1, 32, 16 );
-			// 	var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube } );
-		
-			// 	//
-			// 	renderer = new THREE.WebGLRenderer( { antialias: false } );
-			// 	renderer.setPixelRatio( window.devicePixelRatio );
-			// 	// document.body.appendChild( renderer.domElement );
-			// 	// this.refs.threejs.appendChild( renderer.domElement );
-			// 	var width = window.innerWidth || 2;
-			// 	var height = window.innerHeight || 2;
-			// 	// effect = new THREE.AnaglyphEffect( renderer );
-			// 	// effect.setSize( width, height );
-			// 	//
-			// }
-			
-			export const animate = () => {
-				requestAnimationFrame( animate );
-				 render();
-			}
-
-			function render() {
-				var camera = document.getElementById('bubbleCamera')
-				var timer = 0.0001 * Date.now();
-				let curr = camera.getAttribute("position")
-				let addx = curr.x + (( mouseX - curr.x ) * .05)
-				let addy = curr.y + (( - mouseY - curr.y ) * .05)
-				camera.setAttribute('position', {x: addx, y: addy, z: 5})
-			
-				// camera.lookAt( scene.position );
-				let position = document.querySelector('a-scene').getAttribute('position')
-				camera.setAttribute('look-at', position)
-				for ( var i = 0, il = spheres.length; i < il; i ++ ) {
-					var sphere = spheres[ i ];
-					sphere.setAttribute('position', {x: 5 * Math.cos( timer + i )})
-					sphere.setAttribute('position', {y: 5 * Math.sin( timer + i * 1.1)})
-
-					// sphere.position.x = 5 * Math.cos( timer + i );
-					// sphere.position.y = 5 * Math.sin( timer + i * 1.1 );
-				}
-				//effect.render( scene, camera );
-			}
+module.exports = { initScene, makeBubbles, animate }
