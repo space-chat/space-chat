bb3da94b3655e890b1726b4ea37004f5b2e0ce88
ssundby <stefanie.sundby@gmail.com>
2017-05-12 17:59:24 -0400

    fixed a comment

--- app/sockets.js
+++ app/sockets.js
@@ -22,7 +22,7 @@ export function receiveMessage(clientLang) {
     console.log('incoming message ', messageText, ' in language ', lang)
     // if lang in 'got message' payload matches socket user's language
     if (clientLang === lang && translatedBool) {
-      // speak text from 'got message' payload
+      // find correct voice, speak text from 'got message' payload
       var utterance = new SpeechSynthesisUtterance(messageText)
       utterance.voice = voices.filter(voice => 
         voice.lang.substr(0,2) === clientLang
