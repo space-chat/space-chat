33731bce06e04dde657fb9d83afb01e12f3526de
Nicole Fish <nicole.fish@gmail.com>
2017-05-12 01:13:13 -0400

    change handleClick in Home to add selected language to state; add onClick to react-router Link for dispatching

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,35 +4,42 @@ import { connect } from 'react-redux'
 import { setLanguage } from '../reducers/languageReducer.jsx'
 
 class Home extends Component {
-	constructor() {
-		super()
-    this.state = {}
-    this.onChange = this.onChange.bind(this)
-	}
-
-  onChange(e) {
-    // dispatch action with language
-    this.props.setLanguage(e.target.value)
+  constructor() {
+    super()
+    this.state = {
+      language: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleLanguageChange = this.handleLanguageChange.bind(this)
   }
 
-	render() {
-		return (
-		<div>
+  handleClick(e) {
+    // dispatch action with language from state
+    this.props.setLanguage(this.state.language || 'en')
+  }
+
+  handleLanguageChange(e) {
+    // set selected language on state
+    this.setState({ language: e.target.value })
+  }
+
+  render() {
+    return (
+    <div>
       <h1>SPACE CHAT</h1>
       <form>
         <h3>Select your language:</h3>
-        <select onChange={this.onChange}>
-          <option value="en">English</option>
-          <option value="es">Spanish</option>
+        <select defaultValue='en' onChange={this.handleLanguageChange}>
+          <option value='en'>English</option>
+          <option value='es'>Spanish</option>
         </select>
-        </form>
-        <div>
-          {/* need to pass language as prop to Room */}
-          <Link to="/room" className="btn">Enter Space Chat</Link>
-        </div>
-		</div>
-		)
-	}
+      </form>
+      <div>
+        <Link to='/room' className='btn' onClick={this.handleClick}>Enter Space Chat</Link>
+      </div>
+    </div>
+    )
+  }
 }
 
 export default connect(null, {setLanguage})(Home)
