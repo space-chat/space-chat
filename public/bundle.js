79be2b634a243000a5eebba61a287610ef271239
Elsa Brown <elsambrown@gmail.com>
2017-05-14 00:22:19 -0400

    branch cleaning

--- public/bundle.js
+++ public/bundle.js
@@ -18082,12 +18082,11 @@ var Blossoms = function Blossoms(props) {
       'a-scene',
       null,
       _react2.default.createElement(_AssetLoader2.default, null),
-      _react2.default.createElement('a-box', { id: 'avatar', position: '-1 1.25 -5', rotation: '45 76 100', depth: '3', height: '3', width: '3', material: 'src: #gh', 'normal-texture-repeat': '50', color: 'white' }),
-      _react2.default.createElement('a-box', { id: 'avatar', position: '4 3.25 -10', rotation: '45 76 100', depth: '1.5', height: '1.5', width: '1.5', material: 'src: #cliff', color: 'white' }),
-      _react2.default.createElement('a-box', { id: 'avatar', position: '8 1.25 -6', rotation: '45 100 68', depth: '2', height: '2', width: '2', material: 'src: #deer', 'normal-texture-repeat': '50', color: 'white' }),
+      _react2.default.createElement('a-sphere', { id: 'avatar', position: '-1 1.25 -5', rotation: '45 76 100', radius: '0.15', material: 'src: #blossoms', 'normal-texture-repeat': '50', color: 'white' }),
+      _react2.default.createElement('a-sphere', { id: 'avatar', position: '4 3.25 -10', rotation: '45 76 100', radius: '0.2', material: 'src: #blossoms', color: 'white' }),
       _react2.default.createElement('a-sky', {
         id: 'sky',
-        src: '#fractal' })
+        src: '#blossoms' })
     )
   );
 };
