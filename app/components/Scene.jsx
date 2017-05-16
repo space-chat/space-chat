e7893cb1eec8c10bee2bcd23ac095e61a2683db8
Keziyah <keziyahlewis@gmail.com>
2017-05-16 12:59:40 -0400

    added particle emitter

--- app/components/Scene.jsx
+++ app/components/Scene.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Avatar from './Avatar'
 import AssetLoader from './AssetLoader'
 import glMatrix, {vec3} from 'gl-matrix'
+import ParticleSystem from 'aframe-particle-system-component'
 
 const avatarRadius = 1.75
     , sceneRadius = 8
@@ -46,18 +47,23 @@ const Scene = (props) => {
   let position = vec3.fromValues(0, 1.6, 0)
   centerScene(position, sceneRadius, center)
 
-  const userPositions = users.map((user, i) => positionOfAvatar(i + 1, users.length + 1, center, [0, 0, 0]))
+  const userPositions = users.map((user, i) => positionOfAvatar(i, users.length, center, [0, 0, 0]))
+      , myPosition = userPositions[0]
   console.log('avatar position:', center, "POSITIONS", userPositions)
   return (
       <a-scene vr-mode-ui="enabled: true">
         <AssetLoader />
         <Avatar position="-1.5 1 -4" />
-        <a-sphere id="avatar" position={vecToStr(center)} radius="1.75" material="src: #blossoms" color="white" />
+        {/*<a-sphere id="avatar" position={vecToStr(center)} radius="1.75" material="src: #blossoms" color="white" />*/}
+        <a-entity particle-system="preset: stars" position={vecToStr(myPosition)}></a-entity>
         <a-light color="red" light="color:red;angle:45;type:spot;target:avatar" angle="45" position="-16.717 11.189 17.925" type="spot" target="avatar" rotation="0 -18.73571990077792 -6.245239966925973" scale="1 1 1" visible="true" />
         <a-light color="red" light="angle:45;color:#f0debb;type:spot;target:null" angle="45" type="spot" target="avatar"></a-light>
        
         {
-          userPositions.map(vecToStr).map(position => <a-sphere position={position} radius="1.75" color="white" material="src: #blossoms"/>)
+          userPositions
+            .map(vecToStr)
+            .map((position, index) =>
+              index ? <a-sphere key={position} position={position} radius="1.75" color="white" material="src: #blossoms"/> : null)
         }
         <a-sky
           id="sky"
